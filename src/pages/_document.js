15e9d4064b263b98063210a8f2e1fbf80d9da78b
Sahil-Mhatre14 <sahil81623@gmail.com>
2025-11-29 20:25:48 -0800

    update app name for iOS

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,9 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
+        <meta name="application-name" content="AI Resources Hub" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="AI Resources Hub" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </Head>
